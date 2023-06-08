01dd560f8fc25a156d243e7e91ea9f03ab6d8a56
strodown <strodown@gmail.com>
2023-06-08 13:11:46 +0200

    chore(mosquitto): customize S50mosquitto to S29mosquitto

--- package/mosquitto/mosquitto.mk
+++ package/mosquitto/mosquitto.mk
@@ -113,9 +113,10 @@ define MOSQUITTO_INSTALL_TARGET_CMDS
 endef
 
 ifeq ($(BR2_PACKAGE_MOSQUITTO_BROKER),y)
+# pixL change S50mosquitto to S29mosquitto
 define MOSQUITTO_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/mosquitto/S50mosquitto \
-		$(TARGET_DIR)/etc/init.d/S50mosquitto
+		$(TARGET_DIR)/etc/init.d/S29mosquitto
 endef
 
 define MOSQUITTO_INSTALL_INIT_SYSTEMD
