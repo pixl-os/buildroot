46b602fac2e68c3296bbb9ea55c4c5a1b3244c85
Romain Naour <romain.naour@gmail.com>
2023-09-11 23:09:16 +0200

    package/{glibc, localedef}: bump to version 2.38-13-g92201f16cbcfd9eafe314ef6654be2ea7ba25675
    Enable mathvec explicitly on aarch64(be) since it's now enabled by
    default [1]. aarch64 mathvec requires at gcc-10 but Buildroot already
    provide gcc-11 as minimum version.
    
    Don't use --enable-fortify-source for now in order to keep original
    behavior while doing the glibc version bump (and because some
    architecture doesn't support well fortify-source, i.e Microblaze).
    Postpone this change to a follow up commit.
    
    Keep the "deprecated" libcrypt enabled just in case if some
    application are not yet ready to use an alternative such as libxcrypt.
    
    Security related changes:
    
      CVE-2023-25139: When the printf family of functions is called with a
      format specifier that uses an <apostrophe> (enable grouping) and a
      minimum width specifier, the resulting output could be larger than
      reasonably expected by a caller that computed a tight bound on the
      buffer size.  The resulting larger than expected output could result
      in a buffer overflow in the printf family of functions.
    
    See:
    https://lists.gnu.org/archive/html/info-gnu/2023-07/msg00010.html
    
    Runtime tested with Qemu on Gitlab-ci:
    https://gitlab.com/kubu93/buildroot/-/pipelines/998435203
    https://gitlab.com/buildroot.org/toolchains-builder/-/pipelines/998926028
    
    [1] https://sourceware.org/git/?p=glibc.git;a=commit;h=cd94326a1326c4e3f1ee7a8d0a161cc0bdcaf07e
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/localedef/localedef.mk
+++ package/localedef/localedef.mk
@@ -7,7 +7,7 @@
 # Use the same VERSION and SITE as target glibc
 # As in glibc.mk, generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
-LOCALEDEF_VERSION = 2.37-45-gb4e23c75aea756b4bddc4abcf27a1c6dca8b6bd3
+LOCALEDEF_VERSION = 2.38-13-g92201f16cbcfd9eafe314ef6654be2ea7ba25675
 LOCALEDEF_SOURCE = glibc-$(LOCALEDEF_VERSION).tar.gz
 LOCALEDEF_SITE = $(call github,bminor,glibc,$(LOCALEDEF_VERSION))
 HOST_LOCALEDEF_DL_SUBDIR = glibc
