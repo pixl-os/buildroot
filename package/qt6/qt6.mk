5a3fbe57e56d801ce2af881441d76d6dfcc06f59
Bozo the geek <bozolesv2@gmail.com>
2023-11-25 20:38:28 +0100

    fix(qt6): remove qt.conf if exists (could be generated by qt5)

--- package/qt6/qt6.mk
+++ package/qt6/qt6.mk
@@ -12,3 +12,13 @@ QT6_SITE = https://download.qt.io/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)
 QT6_GIT = git://code.qt.io
 
 include $(sort $(wildcard package/qt6/*/*.mk))
+
+# The file "qt.conf" can be used to override the hard-coded paths especially by QT5
+# we have to remove it to avoid issues in case of QT6
+define QT6_INSTALL_QT_CONF
+	rm -f $(HOST_DIR)/bin/qt.conf
+endef
+
+define QT6_QT_CONF_FIXUP
+	$(QT6_INSTALL_QT_CONF)
+endef
