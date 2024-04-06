670ce08bdce58db6e1f16929f5572570ec17d0c2
Bozo the geek <bozolesv2@gmail.com>
2024-04-06 23:43:20 +0200

    fix(qt5tools): to use QT 5.15.10 also for QT TOOLS
    fix(qt5tools): to use QT 5.15.10 also for QT TOOLS

--- package/qt5/qt5tools/qt5tools.mk
+++ package/qt5/qt5tools/qt5tools.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-QT5TOOLS_VERSION = ddf6686e9bce7a3232af14a2966b4c895e654f02
+# v5.15.10-lts-lgpl
+QT5TOOLS_VERSION = 7918981beb4bcefea4f686370d38c99fcefaf33e
 QT5TOOLS_SITE = $(QT5_SITE)/qttools/-/archive/$(QT5TOOLS_VERSION)
 QT5TOOLS_SOURCE = qttools-$(QT5TOOLS_VERSION).tar.bz2
 
