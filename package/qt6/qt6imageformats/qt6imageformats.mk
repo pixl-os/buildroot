ead0cc8f63053db7abe962b12ffd5047e0a278ef
Sebio <dohkoken@gmail.com>
2024-05-22 17:05:52 +0200

    bump(qt6): update 6.7.1
    need for pcsx2 v1.7.5836

---
+++ package/qt6/qt6imageformats/qt6imageformats.mk
@@ -0,0 +1,51 @@
+################################################################################
+#
+# qt6imageformats
+#
+################################################################################
+
+QT6IMAGEFORMATS_VERSION = $(QT6_VERSION)
+QT6IMAGEFORMATS_SITE = $(QT6_SITE)
+QT6IMAGEFORMATS_SOURCE = qtimageformats-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6IMAGEFORMATS_VERSION).tar.xz
+QT6IMAGEFORMATS_INSTALL_STAGING = YES
+QT6IMAGEFORMATS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+QT6IMAGEFORMATS_LICENSE = \
+	GPL-2.0+ or LGPL-3.0, \
+	GPL-3.0 with exception (tools), \
+	GFDL-1.3 (docs), \
+	BSD-3-Clause
+
+QT6IMAGEFORMATS_LICENSE_FILES = \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GFDL-1.3-no-invariants-only.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt \
+	LICENSES/Qt-GPL-exception-1.0.txt
+
+QT6IMAGEFORMATS_CONF_OPTS = \
+	-GNinja \
+	-DQT_HOST_PATH=$(HOST_DIR) \
+	-DBUILD_WITH_PCH=OFF \
+	-DQT_BUILD_EXAMPLES=OFF \
+	-DQT_BUILD_TESTS=OFF
+
+QT6IMAGEFORMATS_DEPENDENCIES = \
+	host-pkgconf \
+	host-ninja \
+	qt6base
+
+define QT6IMAGEFORMATS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(QT6IMAGEFORMATS_BUILDDIR)
+endef
+
+define QT6IMAGEFORMATS_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(STAGING_DIR) $(BR2_CMAKE) --install $(QT6IMAGEFORMATS_BUILDDIR)
+endef
+
+define QT6IMAGEFORMATS_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(BR2_CMAKE) --install $(QT6IMAGEFORMATS_BUILDDIR)
+endef
+
+$(eval $(cmake-package))
