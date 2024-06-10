e2dd99f5a200896b353f4f380ca4ab198e72e5f7
Sebio <dohkoken@gmail.com>
2024-06-10 15:36:50 +0200

    perf(vulkan-headers): custom pixl-os to buildroot pixl
    update 1.3.270 -> 1.3.273

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VULKAN_HEADERS_VERSION = v1.3.270
+VULKAN_HEADERS_VERSION = v1.3.273
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
