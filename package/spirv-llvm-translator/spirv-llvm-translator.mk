d3c97d5d7c5283cd011dc73dd7a75b95f904fe2c
Sebio <dohkoken@gmail.com>
2024-06-30 08:47:32 +0200

    fix(spirv-llvm-translator): update version and add config

--- package/spirv-llvm-translator/spirv-llvm-translator.mk
+++ package/spirv-llvm-translator/spirv-llvm-translator.mk
@@ -6,10 +6,24 @@
 
 # Generate version string using:
 #   git describe --tags --match 'v15*' --abbrev=40 origin/llvm_release_150
-SPIRV_LLVM_TRANSLATOR_VERSION = v17.0.0
+# pixL modification for llvm & libclc
+SPIRV_LLVM_TRANSLATOR_VERSION = v18.1.0
 SPIRV_LLVM_TRANSLATOR_SITE = $(call github,KhronosGroup,SPIRV-LLVM-Translator,$(SPIRV_LLVM_TRANSLATOR_VERSION))
 SPIRV_LLVM_TRANSLATOR_LICENSE = NCSA
 SPIRV_LLVM_TRANSLATOR_LICENSE_FILES = LICENSE.TXT
+# pixL - add staging
+SPIRV_LLVM_TRANSLATOR_INSTALL_STAGING = YES
+# pixL - add package build
+SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = clang llvm spirv-headers
+SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
+	-DLLVM_BUILD_TOOLS=ON \
+	-DLLVM_DIR=$(HOST_DIR)/lib/cmake/llvm \
+	-DLLVM_SPIRV_BUILD_EXTERNAL=YES \
+	-DLLVM_SPIRV_INCLUDE_TESTS=OFF \
+	-DLLVM_EXTERNAL_PROJECTS="SPIRV-Headers" \
+	-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=$(HOST_DIR)/include
+
+
 HOST_SPIRV_LLVM_TRANSLATOR_DEPENDENCIES = host-clang host-llvm host-spirv-headers
 HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 	-DLLVM_BUILD_TOOLS=ON \
@@ -19,4 +33,6 @@ HOST_SPIRV_LLVM_TRANSLATOR_CONF_OPTS = \
 	-DLLVM_EXTERNAL_PROJECTS="SPIRV-Headers" \
 	-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=$(HOST_DIR)/include
 
+# pixL - add package build
+$(eval $(cmake-package))
 $(eval $(host-cmake-package))
