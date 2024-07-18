331c6c6c6af905f4d64ea28d68c6d525ac000db9
Sebio <dohkoken@gmail.com>
2024-07-18 08:48:06 +0200

    fix(vulkan-headers): hash

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -7,7 +7,7 @@
 # pixL modification
 # Note: changing this version will also affect vulkan-loader and
 # vulkan-tools, so please update those packages as well.
-VULKAN_HEADERS_VERSION = 1.3.288
+VULKAN_HEADERS_VERSION = 1.3.290
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0, MIT
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.md
