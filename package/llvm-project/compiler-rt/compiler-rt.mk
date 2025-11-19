3f256018b0a0ae4eeb1cb091a78d318ca6854867
BOZO THE GEEK <bozolesv2@gmail.com>
2025-11-19 18:46:29 +0100

    chore(llvm-project): bump to 18.1.2 as expected ny mesa3d now to build
    Auto stash before rebase of "mesa3d-24-2.1"

--- package/llvm-project/compiler-rt/compiler-rt.mk
+++ package/llvm-project/compiler-rt/compiler-rt.mk
@@ -18,7 +18,7 @@ COMPILER_RT_INSTALL_TARGET = NO
 
 # pixL Patch for take CMakePolicy.cmake into /host/lib/cmake/llvm
 define COMPILER_RT_CMAKELIST_FIX_POLICY
-	sed -i -r '9s|.*/Modules/CMakePolicy.cmake.*|include($(HOST_DIR)/lib/cmake/llvm/CMakePolicy.cmake|g' $(@D)/CMakeLists.txt
+	sed -i 's|Modules/CMakePolicy.cmake|CMakePolicy.cmake|g' $(@D)/CMakeLists.txt
 endef
 COMPILER_RT_PRE_CONFIGURE_HOOKS= COMPILER_RT_CMAKELIST_FIX_POLICY
 
@@ -26,7 +26,8 @@ COMPILER_RT_CONF_OPTS=-DCOMPILER_RT_STANDALONE_BUILD=OFF \
 	-DCOMPILER_RT_STANDALONE_BUILD=ON \
 	-DCOMPILER_RT_DEFAULT_TARGET_TRIPLE=$(GNU_TARGET_NAME) \
 	-DLLVM_CONFIG_PATH=$(HOST_DIR)/bin/llvm-config \
-	-DCMAKE_MODULE_PATH=$(HOST_DIR)/lib/cmake/llvm
+	-DCMAKE_MODULE_PATH=$(HOST_DIR)/lib/cmake/llvm \
+	-DLLVM_COMMON_CMAKE_UTILS=$(HOST_DIR)/lib/cmake/llvm
 
 # The installation of the target runtime libraries defaults to DESTDIR, however
 # host-clang resources directory needs a link so Clang can find the runtime
