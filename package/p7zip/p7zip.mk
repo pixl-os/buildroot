88f233916ac3b31b820db2acf55735dbac2c53fe
BOZO THE GEEK <bozolesv2@gmail.com>
2026-08-25 00:44:32 +0200

    chore(p7zip): bump to 17.05 (with capacity to manage more formats)

--- package/p7zip/p7zip.mk
+++ package/p7zip/p7zip.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-P7ZIP_VERSION = 17.04
-P7ZIP_SITE = $(call github,jinfeihan57,p7zip,v$(P7ZIP_VERSION))
+P7ZIP_VERSION = 17.05
+P7ZIP_SITE = $(call github,p7zip-project,p7zip,v$(P7ZIP_VERSION))
 P7ZIP_LICENSE = LGPL-2.1+ with unRAR restriction
 P7ZIP_LICENSE_FILES = DOC/License.txt
 P7ZIP_CPE_ID_VENDOR = 7-zip
@@ -19,16 +19,45 @@ endif
 # p7zip buildsystem is a mess: it plays dirty tricks with CFLAGS and
 # CXXFLAGS, so we can't pass them. Instead, it accepts ALLFLAGS_C
 # and ALLFLAGS_CPP as variables to pass the CFLAGS and CXXFLAGS.
+# pixl - add 7z make flag
 define P7ZIP_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" ALLFLAGS_C="$(TARGET_CFLAGS)" \
 		CXX="$(TARGET_CXX)" ALLFLAGS_CPP="$(TARGET_CXXFLAGS)" \
 		LDFLAGS="$(TARGET_LDFLAGS)" \
-		-C $(@D) $(P7ZIP_TARGET)
+		-C $(@D) 7z $(P7ZIP_TARGET)
 endef
 
+# pixl - install 7z and libraries too
 define P7ZIP_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/$(P7ZIP_TARGET) \
 		$(TARGET_DIR)/usr/bin/$(P7ZIP_TARGET)
+	$(INSTALL) -D -m 0755 $(@D)/bin/7z \
+		$(TARGET_DIR)/usr/bin/7z
+	$(INSTALL) -D -m 0755 $(@D)/bin/7z.so \
+		$(TARGET_DIR)/usr/bin/7z.so
+	mkdir -p $(TARGET_DIR)/usr/bin/Codecs
+	$(INSTALL) -D -m 0755 $(@D)/bin/Codecs/Rar.so \
+	    $(TARGET_DIR)/usr/bin/Codecs/Rar.so
+endef
+
+# pixl
+ifeq ($(BR2_PACKAGE_HOST_P7ZIP_7ZA),y)
+HOST_P7ZIP_TARGET = 7za
+else
+HOST_P7ZIP_TARGET = 7zr
+endif
+
+# pixl
+define HOST_P7ZIP_BUILD_CMDS
+        $(HOST_CONFIGURE_OPTS) $(HOST_MAKE_ENV) $(MAKE) -C $(@D) $(HOST_P7ZIP_TARGET)
+endef
+
+# pixl
+define HOST_P7ZIP_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/bin/$(HOST_P7ZIP_TARGET) \
+		$(HOST_DIR)/usr/bin/$(HOST_P7ZIP_TARGET)
 endef
 
 $(eval $(generic-package))
+# pixl
+$(eval $(host-generic-package))
